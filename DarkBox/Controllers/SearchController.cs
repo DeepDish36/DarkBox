dd9ded314da88c41ba513650931666a142aa182a
DeepDish36 <andredeepgomes@gmail.com>
2025-03-17 11:28:47 +0000

    Ver search

---
+++ DarkBox/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using DarkBox.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DarkBox.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public class SearchController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SearchController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Search([FromQuery] string query)
+        {
+            if (string.IsNullOrEmpty(query))
+                return BadRequest("A pesquisa não pode estar vazia.");
+
+            var users = _context.Users
+                .Where(u => u.Username.Contains(query))
+                .Select(u => new { u.UserId, u.Username})
+                .ToList();
+
+            var projects = _context.Projects
+                .Where(p => p.Title.Contains(query))
+                .Select(p => new { p.ProjectId, p.Title })
+                .ToList();
+
+            return Ok(new { users, projects });
+        }
+    }
+}
